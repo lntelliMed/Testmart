c0e13f5fe6adabc7e73118a0b69b0f0f1c50e8c0
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-06-30 09:14:31 -0400

    Add ShopInfo class

---
+++ src/com/intellimed/ShopInfo.java
@@ -0,0 +1,26 @@
+package com.intellimed;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class ShopInfo {
+	
+	@WebMethod
+	@WebResult(partName = "lookupOutput")
+	public String getShopInfo(@WebParam(partName = "lookupInput") String property){
+		String response = "Invalid property";
+		if ("shopInfo".equals(property)){
+			return "Test Mart";
+		} else if ("since".equals(property)){
+			return "since 2012";
+		}
+		
+		return response;
+	}
+}
