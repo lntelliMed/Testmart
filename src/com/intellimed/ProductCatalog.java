dad0d1da2e3f0b537cb93e0ca47fd5e69c8cbc0c
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-06-30 08:40:10 -0400

    Change WebService and WebMethod attribute values

--- src/com/intellimed/ProductCatalog.java
+++ src/com/intellimed/ProductCatalog.java
@@ -4,21 +4,22 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import com.intellimed.business.ProductServiceImpl;
 
-@WebService
+@WebService(name="TestMartCatalog", portName="TestMartCatalogPort", serviceName="TestMartCatalogService",
+			targetNamespace="http://www.testmart.com")
 public class ProductCatalog {
 	private ProductServiceImpl productServiceImpl = new ProductServiceImpl();
 	
-	@WebMethod
+	@WebMethod(action="fetch-categories", operationName="fetchCategories")
 	public List<String> getProductCategories(){
 		return productServiceImpl.getProductCategories();
 	}
 	
-	@WebMethod
+	@WebMethod (exclude = true)
 	public List<String> getProducts(String category){
 		return productServiceImpl.getProducts(category);
 	}
 	
-	@WebMethod
+	@WebMethod (exclude = true)
 	public boolean addProduct(String category, String product) {
 		return productServiceImpl.addProduct(category, product);
 	}
